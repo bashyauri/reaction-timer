7bd36c9b118327578ae55040fd1fe08873381c8a
bashyauri <80903696+bashyauri@users.noreply.github.com>
2022-07-20 12:21:32 +0100

    Reaction Timer Completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
+
+
+<template>
+
+  <div align="center">
+    <h1>Bashar Tech Timer</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <Results v-if="showResults" :score="score" />
+
+  </div>
+
+</template>
 <script>
+import Block from "@/components/Block.vue";
+import Results from "@/components/Results.vue";
 export default {
   name: "App",
   components: {
+    Block,
+    Results,
+
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
 
   }
 
 }
 </script>
-
-<template>
-  <h1>Bashar Tech Timer</h1>
-</template>
 
 <style scoped>
 header {
@@ -38,5 +74,22 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
+}
+
+button {
+  background: blue;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+    <p>Reaction Time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+<script>
+export default {
+    name: "Results",
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Good Touch Bro !!!"
+        } else if (this.score < 400) {
+            this.rank = " Not Bad"
+        } else {
+            this.rank = 'Keep trying '
+        }
+    }
+}
+</script>
+<style>
+.rank {
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
